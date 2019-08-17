d404d6ca8a4bcd7a09b74edd5d94b7fc09002f99
Ross Scarborough <rossscarborough@gmail.com>
2019-08-16 22:14:03 -0400

    Added dependency injection for automapper

---
+++ Src/Template.Infrastructure/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Template.Domain.Models;
+using Template.Infrastructure.Couchbase.Models;
+
+namespace Template.Infrastructure
+{
+    internal class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<SimpleMessage, SimpleMessageDbModel>();
+            CreateMap<SimpleMessageDbModel, SimpleMessage>();
+        }
+    }
+}
--- Src/Template.Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
+++ Src/Template.Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Template.Domain.Couchbase;
+using Template.Infrastructure;
 using Template.Infrastructure.Couchbase;
+using AutoMapper;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -9,6 +11,11 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddSingleton<ISimpleMessageRepository, SimpleMessageRepository>();
+            services.AddTransient(provider => 
+                new MapperConfiguration( cfg =>
+                    {
+                        cfg.AddProfile(new AutoMapperProfile());
+                    }).CreateMapper());
 
             return services;
         }
