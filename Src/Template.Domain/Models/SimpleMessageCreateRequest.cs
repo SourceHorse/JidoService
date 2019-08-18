25e1482b8770bd7bdd7848164f2cf3e5f251bce3
Ross Scarborough <rossscarborough@gmail.com>
2019-08-17 22:45:51 -0400

    Added new request classes

---
+++ Src/Template.Domain/Models/SimpleMessageCreateRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Domain.Models
+{
+    /// <summary>
+    /// For creating a new simple message
+    /// </summary>
+    class SimpleMessageCreateRequest
+    {
+        /// <summary>
+        /// The message title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The message body
+        /// </summary>
+        public string Body { get; set; }
+    }
+}
---
+++ Src/Template.Domain/Models/SimpleMessageUpdateRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Domain.Models
+{
+    /// <summary>
+    /// For updating an existing simple message
+    /// </summary>
+    class SimpleMessageUpdateRequest
+    {
+        /// <summary>
+        /// The message title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The message body
+        /// </summary>
+        public string Body { get; set; }
+    }
+}
