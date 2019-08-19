3c716ddd90233334dcda1b35be2a5d2eda04d350
Ross Scarborough <rossscarborough@gmail.com>
2019-08-18 23:21:49 -0400

    Added validator for update model

---
+++ Src/Template.Api/Validators/SimpleMessageUpdateRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Template.Domain.Models;
+
+namespace Template.Api.Validators
+{
+    public class SimpleMessageUpdateRequestValidator : AbstractValidator<SimpleMessageUpdateRequest>
+    {
+        public SimpleMessageUpdateRequestValidator()
+        {
+            RuleFor(msg => msg.Title).NotNull().NotEmpty();
+            RuleFor(msg => msg.Title).NotNull().NotEmpty();
+        }
+    }
+}
