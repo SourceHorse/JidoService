d665346cbd062614d2956c1acf0d68956187d282
Ross Scarborough <rossscarborough@gmail.com>
2019-08-18 23:17:38 -0400

    Added new validator for simpleMessageCreateRequest

---
+++ Src/Template.Api/Validators/SimpleMessageCreateRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Template.Domain.Models;
+
+namespace Template.Api.Validators
+{
+    public class SimpleMessageCreateRequestValidator : AbstractValidator<SimpleMessageCreateRequest>
+    {
+        public SimpleMessageCreateRequestValidator()
+        {
+            RuleFor(msg => msg.Title).NotNull().NotEmpty();
+            RuleFor(msg => msg.Body).NotNull().NotEmpty();
+        }
+    }
+}
