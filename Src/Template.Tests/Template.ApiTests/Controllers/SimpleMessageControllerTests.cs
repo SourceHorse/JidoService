64cffb4f9888e40a7c553b52e686de33fc8afd31
Ross Scarborough <rossscarborough@gmail.com>
2019-08-21 00:41:37 -0400

    First unit test

--- Src/Template.Tests/Template.ApiTests/Controllers/SimpleMessageControllerTests.cs
+++ Src/Template.Tests/Template.ApiTests/Controllers/SimpleMessageControllerTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Template.Api.Controllers;
 using Template.Domain.Models;
@@ -13,22 +16,37 @@ namespace Template.Tests.Template.ApiTests.Controllers
     public class SimpleMessageControllerTests
     {
         private readonly SimpleMessageController _controller;
-        private readonly Mock<ISimpleMessageService> _simpleMessageServiceMock;
-        private readonly Mock<IValidator<SimpleMessageCreateRequest>> _simpleMessageCreateRequestMock;
-        private readonly Mock<IValidator<SimpleMessageUpdateRequest>> _simpleMessageUpdateRequestMock;
+        private readonly Mock<ISimpleMessageService> _simpleMessageServiceMock = new Mock<ISimpleMessageService>();
+        private readonly Mock<IValidator<SimpleMessageCreateRequest>> _createValidatorMock = new Mock<IValidator<SimpleMessageCreateRequest>>();
+        private readonly Mock<IValidator<SimpleMessageUpdateRequest>> _updateValidatorMock = new Mock<IValidator<SimpleMessageUpdateRequest>>();
 
         public SimpleMessageControllerTests()
         {
-            _controller = new SimpleMessageController()
-            {
+            _controller = new SimpleMessageController(
+                _simpleMessageServiceMock.Object,
+                _createValidatorMock.Object,
+                _updateValidatorMock.Object);
 
-            }
+            
         }
 
         [Fact]
-        public void Create_Success_ReturnsCreatedResultWithObject()
+        public async Task Create_Success_ReturnsCreatedResult()
         {
-            
+            // Arrange
+            var messageCreateObject = new SimpleMessageCreateRequest()
+            {
+                Title = "Test Title",
+                Body = "Test Body"
+            };
+            _createValidatorMock.Setup(x => x.ValidateAsync(It.IsAny<SimpleMessageCreateRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(new FluentValidation.Results.ValidationResult());
+            _simpleMessageServiceMock.Setup(x => x.AddMessage(It.IsAny<SimpleMessageCreateRequest>())).ReturnsAsync(new SimpleMessage());
+
+            // Act
+            var result = await _controller.Create(messageCreateObject);
+
+            // Assert
+            Assert.IsType<CreatedResult>(result);
         }
     }
 }
