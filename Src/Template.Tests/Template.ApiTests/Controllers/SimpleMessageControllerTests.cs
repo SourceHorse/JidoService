9600f4ff5ecc5606c9c072b8abfaae1f19ff0d97
Ross Scarborough <rossscarborough@gmail.com>
2019-08-20 23:42:02 -0400

    Creating project structure for tests

---
+++ Src/Template.Tests/Template.ApiTests/Controllers/SimpleMessageControllerTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Template.Tests.Template.ApiTests.Controllers
+{
+    class SimpleMessageControllerTests
+    {
+    }
+}
