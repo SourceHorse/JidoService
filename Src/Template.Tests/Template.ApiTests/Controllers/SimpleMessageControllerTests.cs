2e10c212eedbbb2914b6f7f7b721d61d22b575a8
Ross Scarborough <rossscarborough@gmail.com>
2019-08-20 23:53:15 -0400

    Added moq

--- Src/Template.Tests/Template.ApiTests/Controllers/SimpleMessageControllerTests.cs
+++ Src/Template.Tests/Template.ApiTests/Controllers/SimpleMessageControllerTests.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Moq;
+using Template.Domain.Models;
+using Template.Domain.Services;
 using Xunit;
 
 namespace Template.Tests.Template.ApiTests.Controllers
 {
-    class SimpleMessageControllerTests
+    public class SimpleMessageControllerTests
     {
+        private readonly Mock<ISimpleMessageService> _simpleMessageServiceMock;
+
+        public SimpleMessageControllerTests()
+        {
+
+        }
+
+        [Fact]
+        public void Create_Success_ReturnsCreatedResultWithObject()
+        {
+            
+        }
     }
 }
