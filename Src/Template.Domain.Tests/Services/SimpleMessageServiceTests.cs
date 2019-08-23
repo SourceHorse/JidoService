047d3657c82431e0115fdb0bf02b6871502bc587
Ross Scarborough <rossscarborough@gmail.com>
2019-08-22 23:54:09 -0400

    Added test for AddMessage method

--- Src/Template.Domain.Tests/Services/SimpleMessageServiceTests.cs
+++ Src/Template.Domain.Tests/Services/SimpleMessageServiceTests.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Moq;
 using Template.Domain.Couchbase;
+using Template.Domain.Models;
 using Template.Domain.Services;
+using Template.Domain.Services.Impl;
+using Template.Infrastructure.Couchbase;
 using Xunit;
 
 namespace Template.Domain.Tests.Services
 {
-    class SimpleMessageServiceTests
+    public class SimpleMessageServiceTests
     {
-        private readonly ISimpleMessageService _service;
+        private readonly SimpleMessageService _service;
         private readonly Mock<ISimpleMessageRepository> _simpleMessageRepositoryMock = new Mock<ISimpleMessageRepository>();
+
+        public SimpleMessageServiceTests()
+        {
+            _service = new SimpleMessageService(_simpleMessageRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task AddMessage_ReturnsSimpleMessage()
+        {
+            // Arrange
+            _simpleMessageRepositoryMock.Setup(x => x
+                .AddMessage(It.IsAny<SimpleMessageCreateRequest>()))
+                .ReturnsAsync(new SimpleMessage());
+
+            // Act
+            var result = await _service.AddMessage(new SimpleMessageCreateRequest());
+
+            // Assert
+            Assert.IsType<SimpleMessage>(result);
+        }
     }
 }
