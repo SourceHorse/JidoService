abccd6fc31dbe557ee2bfc4c803b3697854546b0
Ross Scarborough <rossscarborough@gmail.com>
2019-08-22 23:43:24 -0400

    Added class with dependencies for testing services

---
+++ Src/Template.Domain.Tests/Services/SimpleMessageServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Template.Domain.Couchbase;
+using Template.Domain.Services;
+using Xunit;
+
+namespace Template.Domain.Tests.Services
+{
+    class SimpleMessageServiceTests
+    {
+        private readonly ISimpleMessageService _service;
+        private readonly Mock<ISimpleMessageRepository> _simpleMessageRepositoryMock = new Mock<ISimpleMessageRepository>();
+    }
+}
