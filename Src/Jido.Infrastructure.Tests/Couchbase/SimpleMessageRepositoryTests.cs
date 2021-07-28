dc0ed801456412511033a081f803f13e6868b51d
SourceHorse <rossscarborough@gmail.com>
2021-07-27 23:55:35 -0400

    experimenting with couchbase bucket mocks

--- Src/Jido.Infrastructure.Tests/Couchbase/SimpleMessageRepositoryTests.cs
+++ Src/Jido.Infrastructure.Tests/Couchbase/SimpleMessageRepositoryTests.cs
@@ -30,11 +30,13 @@ namespace Jido.Infrastructure.Tests.Couchbase
         public async Task AddMessage_ReturnsSimpleMessage()
         {
             // Arrange
-            var responseTask = Task.FromResult((IOperationResult<SimpleMessageDbModel>) new SimpleMessageDbModel());
+            var simpleMessageOperationResult = new Mock<IOperationResult<SimpleMessageDbModel>>().Object;
+            var responseTask = Task.FromResult((IOperationResult<SimpleMessageDbModel>)simpleMessageOperationResult);
             _bucketMock.Setup(x => x.InsertAsync(It.IsAny<string>(), It.IsAny<SimpleMessageDbModel>()));
             _bucketMock.Setup(x => x.GetAsync<SimpleMessageDbModel>(It.IsAny<string>())).Returns(responseTask);
             _mapperMock.Setup(x => x.Map<SimpleMessageDbModel>(It.IsAny<SimpleMessageCreateRequest>())).Returns(new SimpleMessageDbModel());
             _mapperMock.Setup(x => x.Map<SimpleMessage>(It.IsAny<SimpleMessageDbModel>())).Returns(new SimpleMessage());
+            _testBucketProviderMock.Setup(x => x.GetBucket()).Returns(_bucketMock.Object);
 
             // Act
             var result = await _repository.AddMessage(new SimpleMessageCreateRequest());
